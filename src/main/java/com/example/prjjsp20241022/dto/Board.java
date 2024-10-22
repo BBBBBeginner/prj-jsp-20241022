85a1570084b088d097bdb90170b751e4511b5969
June <junetiger10@naver.com>
2024-10-22 12:00:12 +0900

    게시물 목록, 새 게시물 작성후 list로 redirect 부분 수정 (Integer id,)

--- src/main/java/com/example/prjjsp20241022/dto/Board.java
+++ src/main/java/com/example/prjjsp20241022/dto/Board.java
@@ -1,13 +1,12 @@
 package com.example.prjjsp20241022.dto;
 
 import lombok.Data;
-import org.apache.ibatis.annotations.Insert;
 
 import java.time.LocalDateTime;
 
 @Data
 public class Board {
-    private Insert id;
+    private Integer id;
     private String title;
     private String content;
     private String writer;
