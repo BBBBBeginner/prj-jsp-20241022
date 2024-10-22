5a760f850dce3075be1c5dd971d3c6af0d2e46ca
June <junetiger10@naver.com>
2024-10-22 10:48:46 +0900

    게시물 목록, 새 게시물 작성후 list로 redirect

--- src/main/java/com/example/prjjsp20241022/dto/Board.java
+++ src/main/java/com/example/prjjsp20241022/dto/Board.java
@@ -1,10 +1,15 @@
 package com.example.prjjsp20241022.dto;
 
 import lombok.Data;
+import org.apache.ibatis.annotations.Insert;
+
+import java.time.LocalDateTime;
 
 @Data
 public class Board {
+    private Insert id;
     private String title;
     private String content;
     private String writer;
+    private LocalDateTime inserted;
 }
