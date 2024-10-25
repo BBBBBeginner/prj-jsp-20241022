c81e74357180df700aeeed22467fded65607c6dc
June <junetiger10@naver.com>
2024-10-25 16:02:18 +0900

    로그인 시 권한 정보도 조회 navbar의 회원목록 버튼 권한에 따라 보이게

--- src/main/java/com/example/prjjsp20241022/dto/Member.java
+++ src/main/java/com/example/prjjsp20241022/dto/Member.java
@@ -3,6 +3,8 @@ package com.example.prjjsp20241022.dto;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
 
 @Data
 public class Member {
@@ -11,4 +13,5 @@ public class Member {
     private String password;
     private String description;
     private LocalDateTime inserted;
+    private List<String> auth;
 }
